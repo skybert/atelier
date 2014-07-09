9aeb1532c04599317d064b65a00ef4a2f619bb9a
Torstein Krause Johansen <torstein.k.johansen@gmail.com>
2014-07-09 21:27:37 +0200

    Added all Atelier web services to the application as Undertow needs this.

--- src/main/java/no/studios/atelier/ws/AtelierWebServices.java
+++ src/main/java/no/studios/atelier/ws/AtelierWebServices.java
@@ -8,20 +8,22 @@ import javax.ws.rs.core.Application;
 /**
  * Registering the root context of all Atelier web services. Using this
  * approach, it's not necessary to set up WS scanning in <code>web.xml</code>.
- * 
+ *
  * @author Torstein Krause Johansen
  * @version 1.0
  */
 @ApplicationPath(WSConstants.PATH_WS_ROOT)
 public class AtelierWebServices extends Application
 {
-
   @Override
   public Set<Class<?>> getClasses()
   {
     HashSet<Class<?>> classes = new HashSet<Class<?>>();
-    classes.add(RootWS.class);
+    classes.add(CustomerOrderWS.class);
     classes.add(CustomerWS.class);
+    classes.add(PostPlaceWS.class);
+    classes.add(ProductTypeWS.class);
+    classes.add(RootWS.class);
     return classes;
   }
 }
