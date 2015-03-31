71769c676adbb1f33e3e125934e506157b152f42
Torstein Krause Johansen <torstein.k.johansen@gmail.com>
2015-03-31 13:43:57 +0200

    Added ISO date filter
    - fixed bug in filter_suppress_none where dates having year < 1900
      produce an error (!)

--- src/main/python/atelier_filters.py
+++ src/main/python/atelier_filters.py
@@ -4,7 +4,7 @@ def filter_suppress_none(value):
     if not value is None:
         if  isinstance(value, basestring):
             return value.decode('utf-8')
-        elif isinstance(value, datetime):
+        elif isinstance(value, datetime) and value.year > 1900:
             return value.strftime("%A %d %B %Y").decode('utf-8')
         else:
             return value
@@ -16,4 +16,13 @@ def filter_object_suppress_none(value):
         return value
     else:
         return ""
+
+
+def filter_iso_date(value):
+    if not value is None and isinstance(value, datetime):
+        return value.strftime("%Y-%m-%d")
+    else:
+        return ""
+
+
 
